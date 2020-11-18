45b3d9489c6ce6d89572988309243031e9d5b41a
swarzchen <aaaa102234@gmail.com>
2020-11-18 22:13:42 +0800

    +

--- Lab4/lab4_3.cpp
+++ Lab4/lab4_3.cpp
@@ -13,6 +13,38 @@ long long dist[N];
 int nj, nr;
 long long now[N];
 
+struct Cmp {
+    bool operator() (Edge& a, Edge& b){
+        int need;
+        int u = a.idx;
+        int min_need1 = 0;
+        int min_need2 = 0;
+        for(auto v:adj[u]){
+            if(open[v.idx] - (now[u] % (open[v.idx] + close[v.idx])) >= take[v.idx]) //開著而且剩下的時間夠通過
+                need = take[v.idx];
+            else if(open[v.idx]- (now[u] % (open[v.idx] + close[v.idx])) >= 0) //開著但是剩下的時間不夠通過
+                need = open[v.idx] - (now[u] % (open[v.idx] + close[v.idx])) + close[v.idx] + take[v.idx];
+            else //現在還關著
+                need = close[v.idx] - (now[u] % (open[v.idx] + close[v.idx]) - open[v.idx]) + take[v.idx];
+            if(min_need1 > need)
+                min_need1 = need;
+        }
+
+        u = b.idx;
+        for(auto v:adj[u]){
+            if(open[v.idx] - (now[u] % (open[v.idx] + close[v.idx])) >= take[v.idx]) //開著而且剩下的時間夠通過
+                need = take[v.idx];
+            else if(open[v.idx]- (now[u] % (open[v.idx] + close[v.idx])) >= 0) //開著但是剩下的時間不夠通過
+                need = open[v.idx] - (now[u] % (open[v.idx] + close[v.idx])) + close[v.idx] + take[v.idx];
+            else //現在還關著
+                need = close[v.idx] - (now[u] % (open[v.idx] + close[v.idx]) - open[v.idx]) + take[v.idx];
+            if(min_need2 > need)
+                min_need2 = need;
+        }
+        return min_need1 > min_need2;
+    }
+};
+
 void init(){
     for(int i = 0;i < nj+1;i++)
         adj[i].clear();
@@ -23,7 +55,7 @@ void spfa(int s){
     fill_n(now, N, 0);
     dist[s] = 0;
     vector<bool> vis(nj+1, false); //從一開始所以要多一
-    priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
+    priority_queue<Edge, vector<Edge>, Cmp<Edge>> pq;
     pq.emplace(0, s);
     int need = 0; // 到下一個點所需要的時間
 
@@ -43,6 +75,7 @@ void spfa(int s){
                 need = close[v.idx] - (now[u] % (open[v.idx] + close[v.idx]) - open[v.idx]) + take[v.idx];
             
             if(dist[v.idx] > dist[u] + need){
+                cout << "u: " << u << " v: " << v.idx << endl;
                 now[v.idx] = now[u] + need;
                 dist[v.idx] = dist[u] + need;
                 pq.emplace(need, v.idx);
