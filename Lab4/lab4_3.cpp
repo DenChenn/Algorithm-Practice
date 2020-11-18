c1bf2cfaff26cc6383b049bc3e363bb82ea79e3a
swarzchen <aaaa102234@gmail.com>
2020-11-18 21:15:43 +0800

    +

--- Lab4/lab4_3.cpp
+++ Lab4/lab4_3.cpp
@@ -1,22 +1,20 @@
 #include <bits/stdc++.h>
-using namespace std;
 #define N 10001
 #define inf 5000000001
-struct Edge{
-    Edge(int a, int b, int c, int d) : idx(a), open(b), close(c), take(d){};
-    int idx;
-    int open;
-    int close;
-    int take;
-};
-
+#define cost first
+#define idx second
+using namespace std;
+using Edge = pair<int, int>;
+int open[100001];
+int close[100001];
+int take[100001];
 vector<Edge> adj[N];
 long long dist[N];
 int nj, nr;
-int now[N];
+long long now[N];
 
 void init(){
-    for(int i = 0;i < nj;i++)
+    for(int i = 0;i < nj+1;i++)
         adj[i].clear();
 }
 
@@ -24,32 +22,30 @@ void spfa(int s){
     fill_n(dist, N, inf);
     fill_n(now, N, 0);
     dist[s] = 0;
-    vector<bool> inq(nj+1, false); //從一開始所以要多一
-    queue<int> q;
-    q.push(s);
-    inq[s] = true;
+    vector<bool> vis(nj+1, false); //從一開始所以要多一
+    priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
+    pq.emplace(open[s]+close[s], s);
     int need = 0; // 到下一個點所需要的時間
 
-    while(!q.empty()){
-        int u = q.front();
-        q.pop();
-        inq[u] = false;
+    while(!pq.empty()){
+        int u = pq.top().idx;
+        pq.pop();
+        if(vis[u])
+            continue;
+        vis[u] = true;
 
         for(auto v:adj[u]){
-            if(v.open - (now[u] % (v.open + v.close)) >= v.take) //開著而且剩下的時間夠通過
-                need = v.take;
-            else if(v.open - (now[u] % (v.open + v.close)) >= 0) //開著但是剩下的時間不夠通過
-                need = v.open - (now[u] % (v.open + v.close)) + v.close + v.take;
+            if(open[v.idx] - (now[u] % (open[v.idx] + close[v.idx])) >= take[v.idx]) //開著而且剩下的時間夠通過
+                need = take[v.idx];
+            else if(open[v.idx]- (now[u] % (open[v.idx] + close[v.idx])) >= 0) //開著但是剩下的時間不夠通過
+                need = open[v.idx] - (now[u] % (open[v.idx] + close[v.idx])) + close[v.idx] + take[v.idx];
             else //現在還關著
-                need = v.close - (now[u] % (v.open + v.close) - v.open) + v.take;
+                need = close[v.idx] - (now[u] % (open[v.idx] + close[v.idx]) - open[v.idx]) + take[v.idx];
             
             if(dist[v.idx] > dist[u] + need){
                 now[v.idx] = now[u] + need;
                 dist[v.idx] = dist[u] + need;
-                if(!inq[v.idx]){
-                    inq[v.idx] = true;
-                    q.push(v.idx);
-                }
+                pq.emplace(open[v.idx]+close[v.idx], v.idx);
             }
         }
     }
@@ -66,7 +62,10 @@ int main() {
         cin >> nj >> nr >> start >> end;
         for(int i = 0;i < nr;i++){
             cin >> u >> v >> x >> y >> t;
-            adj[u].push_back(Edge(v, x, y, t));
+            adj[u].push_back(Edge(x+y, v));
+            open[v] = x;
+            close[v] = y;
+            take[v] = t;
         }
         spfa(start);
         cout << dist[end] << endl;
