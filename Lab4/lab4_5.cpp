48cfb8541202db04d5d1fe74db32aba1b35b74bc
swarzchen <aaaa102234@gmail.com>
2020-12-13 12:15:10 +0800

    lab4 finish

--- Lab4/lab4_5.cpp
+++ Lab4/lab4_5.cpp
@@ -8,8 +8,8 @@ struct Edge{
 };
 struct disSet{
     int dis[N];
-    void init(){
-        for(int i = 0;i < N;i++)
+    void init(int n){
+        for(int i = 1;i <= n;i++)
             dis[i] = i;
     }
 
@@ -30,52 +30,84 @@ struct disSet{
 vector<Edge> E;
 disSet D;
 int vn, en;
-int fir, las;
+int m;
 
 bool Cmp(Edge& e1, Edge& e2){
     return e1.w < e2.w;
 }
 
-int Kruskal(){
-    sort(E.begin(), E.end(), Cmp);
+int Kruskal(int i, int j){
     int cnt = 0;
-    fir = 10001;
-    las = -1;
-    for(auto a: E){
-        if(!D.same(a.from, a.to)){
-            if(a.w > las)
-                las = a.w;
-            if(a.w < fir)
-                fir = a.w;
+    int fir = 10001;
+    int las = -1;
+    for(int x = i;x <= j;x++){
+        int from = E[x].from;
+        int to = E[x].to;
+        if(!D.same(from, to)){
+            if(fir > E[x].w)
+                fir = E[x].w;
+            if(las < E[x].w)
+                las = E[x].w;
             cnt++;
-            D.un(a.from, a.to);
+            D.un(from, to);
         }
     }
-    return cnt;
+    //cout << "cnt: " << cnt << endl;
+    if(cnt != (vn - 1))
+        return -1;
+    else
+        return las - fir;
+}
+
+int solve(){
+    int block = vn - 1, ans;
+    m = 10001;
+    bool find = false;
+    
+    while(block <= en){
+        for(int i = 0;i <= en-block;i++){
+            D.init(vn);
+            //cout << "i: " << i << " j: " << j << endl;
+            ans = Kruskal(i, i+block-1);
+            if(ans != -1){
+                if(m > ans)
+                    m = ans;
+                find = true;
+                //cout << "inin" << endl;
+            }
+        }
+        if(find){
+            break;
+        }
+        else{
+            m = 10001;
+            block++;
+            continue;
+        }
+    }
+    if(m != 10001){
+        return m;
+    }
+    else
+        return -1;
 }
 
 int main () {
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
-    int t;
+    int t, eq;
     cin >> t;
     while(t--){
         E.clear();
-        D.init();
         cin >> vn >> en;
+        D.init(vn);
         for(int i = 1;i <= en;i++){
             Edge e;
             cin >> e.from >> e.to >> e.w;
             E.push_back(e);
         }
         int cnt;
-        cnt = Kruskal();
-        if(cnt == vn-1){
-            cout << "las: " << las << endl;
-            cout << "fir: " << fir << endl;
-            cout << las-fir << endl;
-        }
-        else
-            cout << -1 << endl;
+        sort(E.begin(), E.end(), Cmp);
+        cout << solve() << endl;     
     }
 }
